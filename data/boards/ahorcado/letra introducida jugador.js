fd6dea503a042db366b73c6adc849c866790758e
lluis@protofy.xyz <lluis@protofy.xyz>
2025-07-29 17:16:22 +0200

    bug fixing

--- data/boards/ahorcado/letra introducida jugador.js
+++ data/boards/ahorcado/letra introducida jugador.js
@@ -1,6 +1,8 @@
 const letter = params.letra.toUpperCase()
-await execute_action("letras jugador", {
-	action: "set",
-	key: letter, // key
-	value: board?.['crear palabra'].includes(letter)? "OK": "BAD", // value
-})
+
+	await execute_action("letras jugador", {
+		action: "set",
+		key: letter, // key
+		value: board?.['crear palabra'].includes(letter)? "OK": "BAD", // value
+	})
+
